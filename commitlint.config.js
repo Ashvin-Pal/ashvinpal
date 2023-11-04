fec5ca40de3fe184c0f8e7614aafe6beab00f7c2
Ashvin-Pal <ashvinshash@gmail.com>
2023-11-04 16:27:57 +0100

    build: add prettier config and format all files

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,14 +2,14 @@ const fs = require("fs");
 const path = require("path");
 
 function getAllFolderNames(directoryPath) {
-	try {
-		return fs
-			.readdirSync(path.join(__dirname, directoryPath), { withFileTypes: true })
-			.filter((item) => item.isDirectory())
-			.map(({ name }) => name);
-	} catch (error) {
-		console.log(`Error reading directory: ${error}`);
-	}
+  try {
+    return fs
+      .readdirSync(path.join(__dirname, directoryPath), { withFileTypes: true })
+      .filter((item) => item.isDirectory())
+      .map(({ name }) => name);
+  } catch (error) {
+    console.log(`Error reading directory: ${error}`);
+  }
 }
 
 const appsAllowedScopes = getAllFolderNames("/apps");
@@ -17,12 +17,12 @@ const packagesAllowedScopes = getAllFolderNames("/packages");
 const allScopes = [...appsAllowedScopes, ...packagesAllowedScopes];
 
 module.exports = {
-	extends: ["@commitlint/config-conventional"],
-	/**
-	 * Rules configuration
-	 * @see https://commitlint.js.org/#/reference-rules
-	 */
-	rules: {
-		"scope-enum": [2, "always", allScopes],
-	},
+  extends: ["@commitlint/config-conventional"],
+  /**
+   * Rules configuration
+   * @see https://commitlint.js.org/#/reference-rules
+   */
+  rules: {
+    "scope-enum": [2, "always", allScopes],
+  },
 };
--- packages/eslint-config-ashvinpal/react-internal.js
+++ packages/eslint-config-ashvinpal/react-internal.js
@@ -13,23 +13,23 @@ const project = resolve(process.cwd(), "tsconfig.json");
  */
 
 module.exports = {
-	extends: [
-		"@vercel/style-guide/eslint/browser",
-		"@vercel/style-guide/eslint/typescript",
-		"@vercel/style-guide/eslint/react",
-	].map(require.resolve),
-	parserOptions: {
-		project,
-	},
-	globals: {
-		JSX: true,
-	},
-	settings: {
-		"import/resolver": {
-			typescript: {
-				project,
-			},
-		},
-	},
-	ignorePatterns: ["node_modules/", "dist/", ".eslintrc.js"],
+  extends: [
+    "@vercel/style-guide/eslint/browser",
+    "@vercel/style-guide/eslint/typescript",
+    "@vercel/style-guide/eslint/react",
+  ].map(require.resolve),
+  parserOptions: {
+    project,
+  },
+  globals: {
+    JSX: true,
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {
+        project,
+      },
+    },
+  },
+  ignorePatterns: ["node_modules/", "dist/", ".eslintrc.js"],
 };
